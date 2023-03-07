24dd2a8f6cf34669b58225a1479cb70c4ccc3d54
GeorgiLazarov <lazarov.georgi89@gmail.com>
2023-03-07 15:48:17 +0200

    added db connection (mongo) and some packages

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
-//test
+const path = require('path');
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+mongoose.set('strictQuery', false);
+
+//file with connection string so we will not expose it in github
+const dbconnect = require('./db');
+
+const app = express();
+
+//default view engine
+app.set('view engine', 'pug');
+app.set('views', 'views');
+
+//path to css and js files (frontend)
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+//db connect and start server
+mongoose
+  .connect(dbconnect)
+  .then((result) => {
+    console.log('Connected to DB');
+    app.listen(3000);
+  })
+  .catch((err) => console.log(err));
